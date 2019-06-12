1e4f4f355434cb0cd88fcb43ee44cb96bd16a3d1
adesousa <andre.desousa.95@gmail.com>
2019-06-12 10:17:26 +0200

    Update HomepageSteps.java
    Fixing maximize function: seems no more working in last chrome versions.

--- acceptance-tests/src/test/acceptance/HomepageSteps.java
+++ acceptance-tests/src/test/acceptance/HomepageSteps.java
@@ -29,7 +29,8 @@ public class HomepageSteps {
 	public void beforeScenario() {
 		System.setProperty("webdriver.chrome.driver","/Library/Java/JUNIT/chromedriver");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		// Seems no more working in last Chrome versions
+		// driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
